c5f6ce7da2a669abdccf5cfcbbef01b140ef46ce
Jearranai Jujaroen <jear.jujaroen@gmail.com>
2014-05-02 16:48:17 -0400

    Removed var secretNumber

--- js/app.js
+++ js/app.js
@@ -1,7 +1,56 @@
-
 $(document).ready(function(){
+    /*var secretNumber = Math.ceil(Math.random() * 100);
+        console.log(secretNumber);
+    var secretString = secretNumber.toString();*/
+    var newGame = function() {
+        var secretNumber = Math.ceil(Math.random() * 100);
+            console.log(secretNumber);
+        var secretString = secretNumber.toString();
+        i = 0;
+        $('#count').html('<span>' + i + '</span>');
+        $('#guessList').empty();
+        //guessNumber();
+    }
+    newGame();
     var i = 0;
-    var secretNumber = 89;
+    var comma = function () {
+        if (i !== 0) {
+        $('#guessList').append(", "); 
+        }
+    }
+    var guessNumber = function () {
+        $('.button').on('click', function() {
+            event.preventDefault(); 
+            comma();
+            i++;
+            $('#count').html("<span>" + i + "</span>");
+            $('#guessList').append($('#userGuess').val());
+            var userGuess = $('#userGuess').val();
+                while(userGuess) {
+                    if (userGuess === secretString) {
+                        alert("YOU GOT IT! Click +NEW GAME to start a new game.");
+                        var userGuess = false;
+                    } else if (userGuess <= 20) {
+                        alert("ICEEEEE COLD!!!!! Guess again!");
+                        var userGuess = false;
+                    } else if (userGuess <= 40) {
+                        alert("STILL COLD. Guess again!");
+                        var userGuess = false;
+                    } else if (userGuess <= 60) {
+                        alert("WARMER. Guess again!");
+                        var userGuess = false;
+                    } else if (userGuess <= 80) {
+                        alert("IT'S GETTING HOT IN HERE! Guess again!");
+                        var userGuess = false;
+                    } else if (userGuess <= 100) {
+                        alert("SOOO CLOSE...but keep guessing!");
+                        var userGuess = false;
+                    }
+                
+                }
+        })
+    }
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -13,46 +62,15 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
     
+    $('.new').on('click', function() {
+        newGame();
+
+    })
     $('#userGuess').on('submit', function(event) {
         event.preventDefault();
     })
-    $('.button').on('click', function() {
-        event.preventDefault(); 
-        if (i !== 0) {
-        $('#guessList').append(", "); 
-        }
-        i++;
-        
-        /*--Adding empty() doesn't really seem to make a difference...ask someone about this.--*/
-        //$('#count').empty();
-            //console.log("empty");
-        $('#count').html("<span>" + i + "</span>");
-        $('#guessList').append($('#userGuess').val());
+    guessNumber();
 
-        var userGuess = $('#userGuess').val();
-        while(userGuess) {
-            if (userGuess === 89) {
-                alert("RIGHT ON THE MONEY!");
-                var userGuess = false;
-                console.log("while loop is working");
-            } else if (userGuess <= 20) {
-                alert("ICEEEEE COLD!!!!! Guess again!");
-                var userGuess = false;
-            } else if (userGuess <= 40) {
-                alert("STILL COLD. Guess again!");
-                var userGuess = false;
-            } else if (userGuess <= 60) {
-                alert("WARMER. Guess again!");
-                var userGuess = false;
-            } else if (userGuess <= 80) {
-                alert("IT'S GETTING HOT IN HERE! Guess again!");
-                var userGuess = false;
-            } else if (userGuess <= 100) {
-                alert("SOOO CLOSE...but keep guessing!");
-                var userGuess = false;
-            }
-        }
-    })
 });
 
 
